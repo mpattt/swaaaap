21fd3b598db7287f2a696fff3e48867d01d8e49c
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-08 21:09:13 +0700

    update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -3,16 +3,15 @@ module.exports = {
       mongoUri: 'mongodb://admin:admin@ds133496.mlab.com:33496/swaaap',
       debug: true,
       sessionSecret: 'dev_secret_key',
-  
+
       google: {
         clientID: '41917947091-4o86i624dap6keumdfq1o222c4331f7e.apps.googleusercontent.com',
         clientSecret: 'aLLvyJZrG8ua1UEnX81-qKBu',
-        callbackURL: 'http://localhost:3000/oauth/google/callback'
+        callbackURL: 'https://swaaaap.herokuapp.com/oauth/google/callback'
       },
       facebook: {
         clientID: '378030279305267',
         clientSecret: '6eef122c8309dc308250da794d696bbb',
-        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+        callbackURL: 'https://swaaaap.herokuapp.com/oauth/facebook/callback'
       }
   }
-  
