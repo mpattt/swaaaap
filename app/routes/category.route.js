5a2c27ca174ab154da1c3f57cd338b954884495c
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-14 15:49:35 +0700

    update

---
+++ app/routes/category.route.js
@@ -0,0 +1,6 @@
+const category = require('../controllers/category.controller');
+module.exports = (app) => {
+      const pathcategory = '/api/category';
+      app.get(pathcategory, category.getCategories);
+      app.post(pathcategory+ '/create', category.create);
+}
