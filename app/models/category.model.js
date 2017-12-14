ff7d37c45235534e691f01df716305721870808c
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-14 15:53:24 +0700

    update

--- app/models/category.model.js
+++ app/models/category.model.js
@@ -2,17 +2,16 @@ const mongoose  = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-    categoryname: {
-      type: String,
-      unique: true,
-      //required: true,
-      trim: true
-    },
-    value: String,
-    quantity:{
-      type:Number,
-      default: 0
-    }
+  categoryname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  value: {
+    type: String,
+    required: true,
+    trim: true
+  }
 });
 
 mongoose.model('Category', CategorySchema);
