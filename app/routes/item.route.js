5080d08e116dc8ab12dda3cdebaa9303822225c8
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-14 23:37:21 +0700

    update

--- app/routes/item.route.js
+++ app/routes/item.route.js
@@ -1,7 +1,11 @@
 const item = require('../controllers/item.controller');
+const category = require('../controllers/category.controller');
 module.exports = (app) => {
       const path = '/api/item';
       app.get(path + '/all', item.getItems);
       app.post(path + '/create', item.create);
       app.get(path + '/:id', item.getOne);
+      const pathcategory = '/api/category';
+      app.get(pathcategory, category.getCategories);
+      app.post(pathcategory+ '/create', category.create);
 }
