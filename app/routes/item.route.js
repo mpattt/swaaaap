67ea0131fa662208a9618fa83e1bba5154e6afa8
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-15 09:04:50 +0700

    update

--- app/routes/item.route.js
+++ app/routes/item.route.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
       app.get(path + '/all', item.getItems);
       app.post(path + '/create', item.create);
       app.get(path + '/:id', item.getOne);
-app.put(path + 'reply/:id', item.reply);
+      app.put(path + '/reply/:id', item.reply);
       const pathcategory = '/api/category';
       app.get(pathcategory, category.getCategories);
       app.post(pathcategory+ '/create', category.create);
