26c78fc58f33471cccf0ca72805fcc80118131c0
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-15 09:07:23 +0700

    update

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -48,10 +48,10 @@ exports.getOne = (req, res, next) => {
 
 exports.reply = (req, res, next) => {
         Item.findOneAndUpdate({ _id: req.params.id },{
-          offerusername: req.body.reply,
-          offerimg: req.body.img,
-          offeritemname: req.body.name,
-          offerdescription: req.body.description,
+          offer.offerusername: req.body.reply,
+          offer.offerimg: req.body.img,
+          offer.offeritemname: req.body.name,
+          offer.offerdescription: req.body.description,
         }, (err, data) => {
             if (err) {
                 console.log('Failure: ' + err);
