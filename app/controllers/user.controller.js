9eae3eff669ce473a5d7a4db03846e9b97c61dd7
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-16 08:11:11 +0700

    update

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -110,3 +110,14 @@ exports.saveOAuthUserProfile = (req, profile, done) => {
             }
     });
 }
+
+
+exports.deleteCategory = (req, res, next) => {
+    Post.remove(req.body,(err,post) => {
+                if(err){
+                    return next(err);
+                }else {
+                    res.json(post);
+                }
+        });
+}
--- app/routes/item.route.js
+++ app/routes/item.route.js
@@ -10,5 +10,5 @@ module.exports = (app) => {
       const pathcategory = '/api/category';
       app.get(pathcategory, category.getCategories);
       app.post(pathcategory+ '/create', category.create);
-    //  app.get(pathcategory + '/delete/:id',category.deleteCategory)
+     app.get(pathcategory + '/delete/:id',category.deleteCategory)
 }
