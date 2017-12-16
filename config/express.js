f70e084d6a8fe13e809c01eee42d0b8560ae98b4
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-12-16 07:18:09 +0700

    update

--- config/express.js
+++ config/express.js
@@ -26,7 +26,7 @@ module.exports = function () {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,Accept');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
